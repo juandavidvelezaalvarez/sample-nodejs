6681d2475ded13387471585b1e6660e791c1d617
Juan David Velez Alvarez <juan.velez@and.gov.co>
2021-12-18 13:47:14 -0500

    adaptacion

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,48 @@
 var express = require('express');
 var router = express.Router();
+const https = require('https');
+
+
+const encapsular = (placa) =>
+{
+    return new Promise(
+    function(resolve, reject) {
+        const data = "placa=" + placa;
+        const options = {
+          hostname: 'servicios.laequidadseguros.coop',
+          port: 443,
+          path: '/cotizador/ServletCodigoFasecoldaPlaca',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': data.length
+          }
+        }
+        const req = https.request(options, res => {
+          res.on('data', d => {
+             resolve(d)
+          })
+        })
+        req.on('error', error => {
+           console.error(error)
+        })
+        req.write(data)
+        req.end()
+    }
+);
+}
+
 
 function randomPage(req, res) {
-  res.send('que se cuenta');
+
+  encapsular(req.query.placa).then(function(data) {
+	//        console.log(data);
+    var rpta = data.toString();
+    res.send(rpta);
+  });
+
+
+
 }
 
 router.all('*', randomPage);
